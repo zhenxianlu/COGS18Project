18da5fc012292f88c6e3ca884261ac24ca0f9b5e
zhenxianlu <43864080+zhenxianlu@users.noreply.github.com>
2019-12-11 23:20:56 -0800

    Add files via upload

--- functions.py
+++ functions.py
@@ -3,20 +3,26 @@ import random
 
 X = 500
 Y = 500
+
 class Snake(object):
     def __init__(self):
+        # snake will move randomly when the screen shows up first
         rand_move = random.choice([pg.K_UP,pg.K_DOWN, pg.K_LEFT, pg.K_RIGHT])
         self.dirction = rand_move
         self.sbody = []
         
+        # set the initial snake body length to 1 
         for x in range(1):
             self.addbody()
 
     def addbody(self):
+        # set the initial position of the snake
         x,y = (220,220)
         if self.sbody:
             x,y = (self.sbody[0].x, self.sbody[0].y)
+        # set the snake body size
         sbody = pg.Rect(x,y,20,20)
+        # set the snake position when different key(position) pressed
         if self.dirction == pg.K_LEFT:
             sbody.x -= 20
         elif self.dirction == pg.K_RIGHT:
@@ -28,8 +34,10 @@ class Snake(object):
         self.sbody.insert(0,sbody)
     
     def deletebody(self):
+        # delete snake body
         self.sbody.pop()
 
+    # change snake direction
     def changedirection(self,di):
         LR = [pg.K_LEFT, pg.K_RIGHT]
         UD = [pg.K_UP, pg.K_DOWN]
@@ -39,7 +47,8 @@ class Snake(object):
             if (di in UD) and (self.dirction in UD):
               return
             self.dirction = di
-        
+    
+    # if the snake hit the wall or itself, it is dead
     def dead(self):
         if self.sbody[0].x not in range(X):
             return True
@@ -54,9 +63,11 @@ class Food:
         self.rect = pg.Rect(-20,0,20,20)
 
     def remove(self):
+        # remove the food
         self.rect.x = -20
 
     def pos(self):
+        # set the food size and position
         if self.rect.x == -20:
             position = []
 
@@ -64,4 +75,3 @@ class Food:
                 position.append(pos)
             self.rect.x = random.choice(position)
             self.rect.y = random.choice(position)
-            print(self.rect)
--- script.py
+++ script.py
@@ -12,51 +12,72 @@
 import pygame as pg
 from my_module import functions as fn
 
-
+# initialize screen size
 X = 500
 Y = 500
+
 def main():
     pg.init()
     screen_size = (X,Y)
+    # set the pygame screen size
     screen = pg.display.set_mode(screen_size)
+    # assign the name of the screen
     pg.display.set_caption("Snake")
+    # set the time lock
     clock = pg.time.Clock()
+    # let the score 0 and set the snake alive
     score = 0
     dead = False
 
+    #  call function Snake and Food
     snake = fn.Snake()
     food = fn.Food()
 
     while True:
         for event in pg.event.get():
+            # if pygame is quit
             if event.type == pg.QUIT:
+                # game stops
                 return False
+            # if there is keyboard pressing
             if event.type == pg.KEYDOWN:
+                # change snake direction
                 snake.changedirection(event.key)
                 if event.key == False:
                     return main()
         screen.fill((255,236,50))
 
+        # if the snake is dead
         dead = snake.dead()
         if dead:
+            # the sentence "Game Over!" will show up on the screen
             sfont = pg.font.SysFont('arial',40)
             screen.blit(sfont.render('Game Over!',1,(227,29,18)),(140,200))
+            # as well as the score
             sfont = pg.font.SysFont('arial',20)
             screen.blit(sfont.render('Score:{}'.format(score),1,(0,0,22)),(20,400))
         
+        # if the snake is not dead
         if not dead:
+            # snake will continue moving
             snake.addbody()
             snake.deletebody()
         for rect in snake.sbody:
            pg.draw.rect(screen,(255,153,153),rect,0)
+        # if the snake ate the food
         if food.rect == snake.sbody[0]:
+            # score add 10
             score+=10
+            # add one node to snake body
             snake.addbody()
+            # remove food
             food.remove()
 
+        # draw food
         food.pos()
         pg.draw.rect(screen,(0,0,0),food.rect,0)
 
+        # set the clock tick, the snake moving speed
         pg.display.update()
         clock.tick(6)
 main()
